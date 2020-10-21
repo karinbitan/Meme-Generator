09da58021e9d3d6d17ed38232b5ad165e3823e4d
Karin Bitan <bitankarin@gmail.com>
2020-10-21 14:33:09 +0300

    second commit

--- js/meme-controll.js
+++ js/meme-controll.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function init() {
+    gCanvas = document.querySelector('#meme-canvas');
+    gCtx = gCanvas.getContext('2d');
+    drawImg();
+    renderEditor();
+    renderGallery();
+}
+
+function renderEditor(){
+    var strHTML = '';
+    strHTML = `<input type='text' onchange='getText(this.value)' /> <button onclick="drawText()">Text</button>`
+    document.querySelector('.editor').innerHTML = strHTML;
+}
+
+function renderGallery(){
+    var imgs = gImgs;
+    var strHTML = imgs.map(function(img){
+        return `'<img src='${img.url}' class='img-meme' onclick='setImg(${img.id})' />`;
+    })
+    document.querySelector('.gallery').innerHTML = strHTML.join('');
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,66 @@
 'use strict';
+var gCanvas;
+var gCtx;
+
+var gImgs = [
+    { id: 1, url: 'img/1.jpg' },
+    { id: 2, url: 'img/2.jpg' }
+
+];
+
+var gMeme =
+{
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'Write your text',
+            size: 20,
+            align: 'left',
+            strokColor: 'black',
+            fillColor: 'white',
+            font: 'Impact'
+        }
+    ]
+}
+
+
+
+function getImgById(imgId) {
+    var img = gImgs.find(function (img) {
+        return imgId === img.id;
+    })
+    return img;
+}
+
+function getText(value) {
+    gMeme.lines[0].txt = value;
+}
+
+function setImg(imgID) {
+    gMeme.selectedImgId = imgID;
+    drawImg();
+}
+
+function drawImg() {
+    var imgId = gMeme.selectedImgId;
+    var selectedImg = getImgById(imgId);
+    var img = new Image()
+    img.src = selectedImg.url;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    }
+}
+
+function drawText(x = 70, y = 100) {
+    var text = gMeme.lines[0].txt;
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = 'white';
+    gCtx.lineWidth = '2'
+    gCtx.font = '48px Impact'
+    gCtx.textAlign = 'start'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+
